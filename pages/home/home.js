1f2bc54a9db23efcff241b86537353dd0c80239f
wayne <www_wayne_001@163.com>
2018-09-26 15:13:45 +0800

    英雄列表更新以及咨询页面UI设置

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,80 +5,107 @@ Page({
    * 页面的初始数据
    */
   data: {
-    powerHeroArry:[],
+    powerHeroArry: [],
     minjieHeroArry: [],
     zhiliHeroArry: [],
+    totalArray:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.setData({
-      powerHeroArry: [{ name: '末日', imagePath: '/sources/icons/mori.jpeg' }, { name: '电狗', imagePath: '/sources/icons/diangou.jpeg' }, { name: '军团', imagePath: '/sources/icons/juntuan.jpeg' }, { name: '小小', imagePath: '/sources/icons/xiaoxiao.jpeg' }, { name: '炼魂人', imagePath: '/sources/icons/liehunren.jpeg' }],
-
-      minjieHeroArry: [{ name: '幽鬼', imagePath: '/sources/icons/yougui.jpg' },{ name: '幻影刺客', imagePath: '/sources/icons/huanyingcike.jpg' }, { name: '熊战士', imagePath: '/sources/icons/xiongzhanshi.jpg' }, { name: '嗜血狂魔', imagePath: '/sources/icons/shixuekuangmo.jpg' }, { name: '巨魔战将', imagePath: '/sources/icons/jumozhanjiang.jpg' }, { name: '剧毒术士', imagePath: '/sources/icons/judushushi.jpg' }, { name: '狙击手', imagePath: '/sources/icons/jujishou.jpg' }],
-
-      zhiliHeroArry: [{ name: '瘟疫法师', imagePath: '/sources/icons/wenyifashi.jpeg' }, { name: '修补匠', imagePath: '/sources/icons/xiubujiang.jpeg' }, { name: '祈求者', imagePath: '/sources/icons/qiqiuzhe.jpeg' }, ],
-
-    })
+  onLoad: function(options) {
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+    this.getHerosList();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   },
 
-  enterDetail:function(e){
+  getHerosList: function() {
+var that = this;
+    wx.request({
+      url: 'https://wycode.cn/web/api/public/dota/heroes',
+      success:function(res){
+        console.log('getHerosList->',res);
+        if (res.data.success){
+          that.data.totalArray = res.data.data;
+          var arryList1 = new Array();
+          var arryList2 = new Array();
+          var arryList3 = new Array();
+          for (var i = 0; i < that.data.totalArray.length; i++) {
+            console.log('array->',i);
+            if (that.data.totalArray[i].type == '力量'){
+              arryList1.unshift(that.data.totalArray[i]); 
+            } else if (that.data.totalArray[i].type == '敏捷'){
+              arryList2.unshift(that.data.totalArray[i]); 
+            }else{
+              arryList3.unshift(that.data.totalArray[i]); 
+            }
+          }
+          that.setData({
+            powerHeroArry: arryList1,
+            minjieHeroArry: arryList2,
+            zhiliHeroArry: arryList3,
+          })
+        }
+      },
+      fail:function(res){
+        console.log('获取列表失败!');
+      },
+    })
+  },
+
+  enterDetail: function(e) {
 
     wx.navigateTo({
-      url: '/pages/home/hero-detail/hero-detail' + 
-      '?name=' + e.currentTarget.dataset.item.name + 
-      '&imagePath=' + e.currentTarget.dataset.item.imagePath,
+      url: '/pages/home/hero-detail/hero-detail' +
+        '?name=' + e.currentTarget.dataset.item.name +
+        '&imagePath=' + e.currentTarget.dataset.item.imageUrl,
     })
   },
 
