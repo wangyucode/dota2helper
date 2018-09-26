85085e2803b06287db70599fd4491a7b57ae3433
wayne <www_wayne_001@163.com>
2018-09-26 21:21:12 +0800

    主题 icon

--- pages/home/home.js
+++ pages/home/home.js
@@ -14,7 +14,9 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {},
+  onLoad: function(options) {
+    this.getHerosList();
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -27,7 +29,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-    this.getHerosList();
+    
   },
 
   /**
