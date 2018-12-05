ea58de50524bd40cf98a1f9c44c44418501ea88e
wycode <www_wayne_001@163.com>
2018-12-05 17:45:54 +0800

    A杖加成效果

--- pages/tools/tool-jianjie/a-zhang/a-zhang.js
+++ pages/tools/tool-jianjie/a-zhang/a-zhang.js
@@ -5,13 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    heros:[]
+    heros: [],
+    upgradeSkill: [],
+    addSkill: [],
+    otherSkill: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.getNoEffectHero();
     this.getEffectAbility();
   },
@@ -19,55 +22,55 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
-  getNoEffectHero:function(){
+  getNoEffectHero: function() {
     var that = this;
-    
+
     wx.request({
       url: 'https://wycode.cn/web/api/public/dota/noAzhangHeros',
       success: function(res) {
@@ -81,21 +84,56 @@ Page({
     })
   },
 
-  getEffectAbility:function(){
+  getEffectAbility: function() {
     var that = this;
     wx.showLoading({
       title: '请稍后...',
       mask: true
     })
+
+    wx.request({
+      url: 'https://wycode.cn/web/api/public/dota/AzhangEffects',
+      success: function(res) {
+        console.log('getEffectAbility->', res);
+        if (res.data.success) {
+          that.setData({
+            upgradeSkill: res.data.data.filter((skill) => {
+              return skill.type == 1
+            }),
+            addSkill: res.data.data.filter((skill) => {
+              return skill.type == 2
+            }),
+            otherSkill: res.data.data.filter((skill) => {
+              return skill.type == 3
+            })
+          })
+        }
+      },
+      complete: function() {
+        wx.hideLoading()
+      }
+
+    })
   },
 
 
-  clickNoEffectHero:function(e){
+  clickNoEffectHero: function(e) {
     wx.navigateTo({
       url: '/pages/home/hero-detail/hero-detail' +
         '?name=' + e.currentTarget.dataset.item.name +
         '&imagePath=' + encodeURIComponent(e.currentTarget.dataset.item.imageUrl) +
         '&icon=' + encodeURIComponent(e.currentTarget.dataset.item.icon)
+    })
+  },
+
+
+  skillClick:function(e){
+    var hero = e.currentTarget.dataset.hero
+    wx.navigateTo({
+      url: '/pages/home/hero-detail/hero-detail' +
+        '?name=' + hero.name +
+        '&imagePath=' + encodeURIComponent(hero.imageUrl) +
+        '&icon=' + encodeURIComponent(hero.icon)
     })
   }
 })
