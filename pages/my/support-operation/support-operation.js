d81c1c093caadf0293d294de061fad2540420eb5
yu.wang <wangyu@wycode.cn>
2022-10-22 01:07:49 +0800

    remove reward

--- pages/my/support-operation/support-operation.js
+++ pages/my/support-operation/support-operation.js
@@ -1,65 +1,6 @@
 //pages/my/support-operation/support-operation.js
-const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
     return {
       title: "查英雄、技能、物品、天梯、资讯？点这里！",
@@ -67,13 +8,4 @@ Page({
       imageUrl: "/assets/logo.png"
     }
   },
-
-
-
-  toReward: function () {
-    wx.previewImage({
-      urls: [app.globalData.serverHost + '/upload/image/fish/reward.jpg'],
-    })
-  }
-
 })
